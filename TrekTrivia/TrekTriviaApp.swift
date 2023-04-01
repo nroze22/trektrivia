1aa0a833f89518b03b89f05dd93e138d52e46fee
nroze22 <nroze22@gmail.com>
2023-03-31 19:57:37 -0700

    Initial Commit

---
+++ TrekTrivia/TrekTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekTriviaApp.swift
+//  TrekTrivia
+//
+//  Created by Nathan Rozendaal on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TrekTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
